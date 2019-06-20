c235419a98a89aeb6240a9afbebb6789787f922e
mengmingxiu <mengmingxiu@100tal.com>
2019-06-20 11:58:45 +0800

    mod

--- src/components/NameCard.js
+++ src/components/NameCard.js
@@ -7,7 +7,7 @@ class NameCard extends React.Component {
         <h4 className="alert-=heading">{name}</h4>
         <ul>
           <li>{tel}</li>
-          <li>{isHuman ? "人类" : "外星人"}</li>
+          <li>{isHuman ? "他是人类" : "他是外星人"}</li>
           <li>{tags.map((item, idx) => <span className="badge badge-pill badge-primary" key={idx}>{item}</span>)}</li>
         </ul>
       </div>
